0f35302c21283f6915df0247ae15c1331aefadba
879141253@qq.com <879141253@qq.com>
2017-06-06 01:29:34 +0800

    chapter 11

---
+++ names/daytimetcpsrv2.c
@@ -0,0 +1,25 @@
+#include "unp.h"
+#include <time.h>
+
+int main(int argc, char **argv){
+	int listenfd, connfd;
+	socklen_t len;
+	char buff[MAXLINE];
+	time_t ticks;
+	struct sockaddr_storage cliaddr;
+	if(argc == 2)
+		listenfd = tcp_listen(NULL, argv[1], &addrlen);
+	else if(argc == 3)
+		listenfd = tcp_listen(argv[1], argv[2], &addrlen);
+	else
+		err_quit("unsage: daytimetcpsrv2 [<host>] <service or port>");
+	for(;;){
+		len = sizeof(cliaddr);
+		connfd = accept(listenfd, (SA*)&cliaddr, &len);
+		printf("connection from %s\n", sock_ntop((SA*)&cliaddr, len));
+		ticks = time(NULL);
+		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+		write(connfd, buff, strlen(buff));
+		close(connfd);
+	}
+}
